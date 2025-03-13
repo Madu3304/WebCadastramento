628262758d0f039746aad45614060f9172a44943
maria.souza <maria.souza@fulltechpro.com.br>
2025-03-13 15:12:09 -0300

    #357 - Adicionando models

---
+++ Models/CadeiranteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CadeiranteModel
+{
+	public Cadeirante()
+	{
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public dateTime DataNascimento { get; set; }
+        public string? Email { get; set; }
+        public string? TamanhoBlusa { get; set; }
+        public string? Distancia { get; set; }
+        public string? UltimaCorrida { get; set; }
+     }
+}
